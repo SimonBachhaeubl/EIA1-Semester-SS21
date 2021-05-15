60beacbd67f1a0c0057c58bbd55982a909f5662d
Simon Bachhäubl <simon.bachhaeubl@hs-furtwangen.de>
2021-05-15 21:59:46 +0200

    Aufgabe8 endlich

---
+++ Aufgaben/Aufgabe8/script.js
@@ -0,0 +1,61 @@
+"use strict";
+window.addEventListener("load", function () {
+    document.querySelector("#b1").addEventListener("click", function () { playsample("A.mp3"); });
+    document.querySelector("#b2").addEventListener("click", function () { playsample("C.mp3"); });
+    document.querySelector("#b3").addEventListener("click", function () { playsample("F.mp3"); });
+    document.querySelector("#b4").addEventListener("click", function () { playsample("G.mp3"); });
+    document.querySelector("#b5").addEventListener("click", function () { playsample("hihat.mp3"); });
+    document.querySelector("#b6").addEventListener("click", function () { playsample("kick.mp3"); });
+    document.querySelector("#b7").addEventListener("click", function () { playsample("laugh-1.mp3"); });
+    document.querySelector("#b8").addEventListener("click", function () { playsample("laugh-2.mp3"); });
+    document.querySelector("#b9").addEventListener("click", function () { playsample("snare.mp3"); });
+    document.querySelector("#play").addEventListener("click", playorstop);
+    document.querySelector("#trash").addEventListener("click", deletebeat);
+    document.querySelector("#record").addEventListener("click", recordBeat);
+    document.querySelector("#Remix").addEventListener("click", remixBeat);
+    var beatInt = undefined;
+    var beats = ["kick.mp3", "snare.mp3", "hihat.mp3"];
+    var recording = false;
+    function recordBeat() {
+        if (recording) {
+            recording = false;
+        }
+        else {
+            recording = true;
+        }
+    }
+    function playsample(audios) {
+        if (recording) {
+            beats.push(audios);
+        }
+        var sound = new Audio(audios);
+        sound.play();
+    }
+    function playorstop() {
+        var playorstopbtn = document.querySelector("#play");
+        if (playorstopbtn.className == "fas fa-play") {
+            playorstopbtn.className = "fas fa-stop";
+            playbeat();
+        }
+        else {
+            playorstopbtn.className = "fas fa-play";
+            clearInterval(beatInt);
+        }
+    }
+    function deletebeat() {
+        beats = [];
+    }
+    function playbeat() {
+        var index = 0;
+        beatInt = setInterval(function () {
+            playsample(beats[index]);
+            index += 1;
+            if (index >= beats.length) {
+                index = 0;
+            }
+        }, 600);
+    }
+    function remixBeat() {
+    }
+});
+//# sourceMappingURL=script.js.map
---
+++ Aufgaben/Aufgabe8/script.ts
@@ -0,0 +1,69 @@
+"use strict";
+window.addEventListener("load", function () {
+    document.querySelector("#b1").addEventListener("click", function (): void { playsample("A.mp3"); });
+    document.querySelector("#b2").addEventListener("click", function (): void { playsample("C.mp3"); });
+    document.querySelector("#b3").addEventListener("click", function (): void { playsample("F.mp3"); });
+    document.querySelector("#b4").addEventListener("click", function (): void { playsample("G.mp3"); });
+    document.querySelector("#b5").addEventListener("click", function (): void { playsample("hihat.mp3"); });
+    document.querySelector("#b6").addEventListener("click", function (): void { playsample("kick.mp3"); });
+    document.querySelector("#b7").addEventListener("click", function (): void { playsample("laugh-1.mp3"); });
+    document.querySelector("#b8").addEventListener("click", function (): void { playsample("laugh-2.mp3"); });
+    document.querySelector("#b9").addEventListener("click", function (): void { playsample("snare.mp3"); });
+    document.querySelector("#play").addEventListener("click", playorstop);
+    document.querySelector("#trash").addEventListener("click", deletebeat);
+    document.querySelector("#record").addEventListener("click", recordBeat);
+    document.querySelector("#Remix").addEventListener("click", remixBeat);
+
+    
+    let beatInt = undefined;
+    let beats = ["kick.mp3", "snare.mp3", "hihat.mp3"];
+    let recording = false;
+    function recordBeat() {
+        if (recording) {
+            recording = false;
+        }
+        else {
+            recording = true;
+        }
+    }
+    function playsample(audios): void {
+        if (recording) {
+            beats.push(audios);
+        }
+        const sound = new Audio(audios);
+        sound.play();
+    }
+    function playorstop(): void {
+        let playorstopbtn = document.querySelector("#play");
+        if (playorstopbtn.className == "fas fa-play") {
+            playorstopbtn.className = "fas fa-stop";
+            playbeat();
+        }
+        else {
+            playorstopbtn.className = "fas fa-play";
+            clearInterval(beatInt);
+        }
+    }
+    function deletebeat(): void {
+        beats = [];
+    }
+    function playbeat(): void {
+        var index = 0;
+        beatInt = setInterval(function () {
+            playsample(beats[index]);
+            index += 1;
+            if (index >= beats.length) {
+                index = 0;
+            }
+        }, 600);
+    }
+
+    function remixBeat(): void {
+
+
+
+
+    }
+    
+});
+
